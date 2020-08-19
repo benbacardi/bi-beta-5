25dbf9f99b5af08dd05942f04afe7caefafc575b
Ben Cardy <ben.cardy@ocado.com>
2020-08-19 14:52:34 +0100

    Initial Commit

---
+++ Shared/Bricking_ItApp.swift
@@ -0,0 +1,20 @@
+//
+//  Bricking_ItApp.swift
+//  Shared
+//
+//  Created by Ben Cardy on 19/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Bricking_ItApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
